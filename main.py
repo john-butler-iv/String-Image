30886698ebbd1c84422375f2bb61373900d7b992
relicanth56 <relicanth56@gmail.com>
2020-12-18 21:52:35 -0600

    simplified avg_brightness and saved output image

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from sys import argv
-from PIL import Image, ImageFilter, ImageDraw, ImageStat
+from PIL import Image, ImageDraw, ImageStat
 from random import randint
-# from numpy import sign
 from math import sqrt, sin, cos, pi
-
-
-def sign(val):
-    if val < 0:
-        return -1
-    if val > 0:
-        return 1
-    return 0
+from functools import reduce
 
 
 def get_pins_circle(total_pins, size):
@@ -37,10 +29,6 @@ def get_pins_square(pins_per_side, size):
     return pins
 
 
-def avg(rgb):
-    return rgb[0] + rgb[1] + rgb[2]
-
-
 def dist(a, b):
     dx = b[0] - a[0]
     dy = b[1] - a[1]
@@ -50,68 +38,10 @@ def dist(a, b):
 def pos_equals(pos0, pos1): return pos0[0] == pos1[0] and pos0[1] == pos1[1]
 
 
-def _line_gen_x(pos0, pos1, m):
-    b = pos0[1] - m * pos0[0]
-
-    dx = sign(pos1[0] - pos0[0])
-    x = pos0[0]
-    while x != pos1[0]:
-        yield (x, m * x + b)
-        x += dx
-
-
-def _line_gen_y(pos0, pos1, m):
-    b = pos0[0] - m * pos0[1]
-
-    dy = sign(pos1[1] - pos0[1])
-    y = pos0[1]
-    while y != pos1[1]:
-        yield (m * y + b, y)
-        y += dy
-
-
-def line_gen(pos0, pos1):
-    if pos1[0] == pos0[0]:
-        return _line_gen_y(pos0, pos1, 0)
-    m = (pos1[1] - pos0[1]) / (pos1[0] - pos0[0])
-
-    if m <= 1:
-        return _line_gen_x(pos0, pos1, m)
-    return _line_gen_y(pos0, pos1, 1/m)
-
-
 def avg_brightness(img, pos0, pos1):
-    # if (pos0[0] == pos1[0] and (pos0[0] == 0 or pos0[0] == img.size[0] - 1)) or (pos0[1] == pos1[1] and (pos0[1] == 0 or pos0[1] == img.size[1] - 1)):
-    #    return (img.size[0] + img.size[1]) * 255 * 3
-    # curr_pos = pos0
-    # dx = sign(pos1[0] - pos0[0])
-    # dy = sign(pos1[1] - pos0[1])
-
-    brightness = 0
-    total = 0
-
-    for curr_pos in line_gen(pos0, pos1):
-        pix = img.getpixel(curr_pos)
-        brightness += avg(pix)
-        total += 1
-
-    return brightness / total
-
-def draw_line(img, pos0, pos1, color):
-    curr_pos = pos0
-    dx = sign(pos1[0] - pos0[0])
-    dy = sign(pos1[1] - pos0[1])
-
-    while not pos_equals(curr_pos, pos1):
-        img.putpixel(curr_pos, color)
-
-        new_x = (curr_pos[0] + dx, curr_pos[1])
-        new_y = (curr_pos[0], curr_pos[1] + dy)
-
-        if (dist(new_x, pos1) < dist(new_y, pos1)):
-            curr_pos = new_x
-        else:
-            curr_pos = new_y
+    mask = Image.new('1', img.size)
+    ImageDraw.Draw(mask).line([pos0, pos1], fill=1)
+    return sum(ImageStat.Stat(img, mask).mean)
 
 
 def find_next_pin(src_img, pins, curr_pin, taken):
@@ -132,16 +62,19 @@ def find_next_pin(src_img, pins, curr_pin, taken):
             print(curr_pin)
     return best_pin
 
+
 def total_brightness(img):
-    mean = ImageStat.Stat(img).mean
-    #print(mean)
-    return mean[0] + mean[1] + mean[2]
+    return sum(ImageStat.Stat(img).mean)
+
 
 def print_usage(index):
-    # there cannot be an error with index 0, that's the name of the file
+    # there cannot be an error with index 0, that's the python file
     if(index):
-        print('error with {} argument of {}.'.format(index+1, **argv))
-    print('Usage: {} <inputpath> <outputpath> [-t <minimum brightness tollerance factor>] [-p <# pins>] [-s]'.format(argv[0]))
+        args = ' '.join(map(str, argv))
+        print('error with {} argument of '.format(index+1) + args)
+    print(
+        'Usage: {} <inputpath> <outputpath> [-o <image output file>] [-t <minimum brightness tollerance factor>] [-p <# pins>] [-s]'.format(argv[0]))
+
 
 def parse_opts():
     if len(argv) == 1:
@@ -149,9 +82,9 @@ def parse_opts():
         return None
 
     # records valid options and whether they have a parameter
-    valid_opts = {"-t":True, "-p":True, "-s":False}
+    valid_opts = {"-o": True, "-t": True, "-p": True, "-s": False}
     # value to be returned. A dictionary with the options and their values. These are the default values
-    opts = {'-t':'0.73', '-p': '75', '-s': False}
+    opts = {'-o': None, '-t': '0.73', '-p': '74', '-s': False}
 
     # records how many arguments have been passed without a specified option (0 <=> input, 1 <=> output, 2+ <=> error)
     file = 0
@@ -193,15 +126,15 @@ if __name__ == '__main__':
 
     pins = []
     if opts['-s']:
-        pins = get_pins_square(TOTAL_PINS / 4, size)
+        pins = get_pins_square(int(TOTAL_PINS / 4), size)
     else:
         pins = get_pins_circle(TOTAL_PINS, size)
+
+    times_used = [0] * len(pins)
 
     print(list(enumerate(pins)), sep='\n')
 
     midpt = (int(size[0] / 2), int(size[1] / 2))
-
-
 
     taken = set()
 
@@ -215,15 +148,22 @@ if __name__ == '__main__':
         curr_b = 255*3
         i = 0
         while curr_pin != None and TOLLERANCE * curr_b > target_brightness:
-            src_draw.line(pins[prev_pin] + pins[curr_pin], fill=(255, 255, 255))
+            times_used[curr_pin] += 1
+            src_draw.line(pins[prev_pin] + pins[curr_pin],
+                          fill=(255, 255, 255))
             out_draw.line(pins[prev_pin] + pins[curr_pin], fill=(0, 0, 0))
             taken.add((prev_pin, curr_pin))
             taken.add((curr_pin, prev_pin))
             curr_b = total_brightness(out_img)
-            print(i, '{:.2f} / {:.2f} = {:.2f}, target {:.2f}'.format(target_brightness, curr_b, target_brightness / curr_b, TOLLERANCE))
+            print(i, '{:.0f} / {:.0f} = {:.3f}, target {:.3f}'.format(
+                target_brightness, curr_b, target_brightness / curr_b, TOLLERANCE))
             i += 1
-            print(pins[curr_pin], file=f)
+            print((curr_pin, pins[curr_pin]), file=f)
             prev_pin = curr_pin
             curr_pin = find_next_pin(src_img, pins, curr_pin, taken)
 
+    #print(list(reduce(lambda x, y: max(x, y), times_used)))
+    print(max(times_used), 'is the most one pin is used')
     out_img.show()
+    if opts['-o']:
+        out_img.save(opts['-o'])
